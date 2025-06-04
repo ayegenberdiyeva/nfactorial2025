27907abc8f7d66a9823b5f597ba9bcf6f48146ab
Amina <aminayegnb@gmail.com>
2025-06-04 06:31:03 +0500

    task2

---
+++ task2/ToDoApp/ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Amina Yegenberdiyeva on 04.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ task2/ToDoApp/ToDoApp/View/MainTableView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct MainTableView: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            
+            ProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: "person.circle")
+                }
+        }
+    }
+}
